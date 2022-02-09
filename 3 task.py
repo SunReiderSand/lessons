623a04a56addab6fee7bf0db4c6302b5673ae7c1
SunReider <sunescanorffdp@gmail.com>
2022-02-09 15:23:35 +0600

    Готовый вариант

---
+++ 3 task.py
@@ -0,0 +1,8 @@
+n = int(input("Введите число n: "))
+# преобразуем. В строку
+# Для того чтобы число принялось как строка
+send = str(n)
+s1 = send + send
+s2 = send + send + send
+comp = n + int(t1) + int(t2)
+print("Результат равен:", comp)
